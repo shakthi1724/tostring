42f311447c8a4d17d20c432e126d406bf45e6677
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:13:22 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=[1,2,3,4,5,6];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.toString();
+console.log("Method:fabevy.toString();");
+console.log("Change in array:",a);
+console.log("Length of array after toString(); is ",a.length);
